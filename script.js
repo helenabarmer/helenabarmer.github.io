d3e310844f0913a20bce1efea7cbd3f20d0f1554
Produkten <Prototyp av produkten>
2019-05-29 21:09:50 +0200

    New upload

--- script.js
+++ script.js
@@ -1,6 +1,40 @@
-const arrayMustHave[]
-const arrayShouldHave[]
-const arrayCouldHave[]
-const arrayWontHave []
+const arrayDoing = ["GitHub", "Java", "Coffee", "Meetups", "Testing", "Bugs"]
+
+function getArrayDoing(){
+    const result = arrayDoing[Math.floor(Math.random() * arrayDoing.length)];
+    return result;}
+
+function displayArray(){
+    const result = document.getElementById('displayDoing').innerText= getArrayDoing();
+    return result;}
+
+function generateFunction(){
+    const result = document.getElementById('generate').addEventListener("click", displayArray);
+    return result;
+}
+
+
+    
+
+    
+
+    
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
